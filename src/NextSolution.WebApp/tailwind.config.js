f3b6b250e57a2a500e53b269535ce218c4ea4459
Prince Owusu <princeowusu.272@gmail.com>
2023-08-09 17:43:02 +0000

    feat(web): ✨ Install NextUI

--- src/NextSolution.WebApp/tailwind.config.js
+++ src/NextSolution.WebApp/tailwind.config.js
@@ -1,15 +1,20 @@
+const { nextui } = require("@nextui-org/react");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: "class",
   content: [
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
 
     // Or if using `src` directory:
-    "./src/**/*.{js,ts,jsx,tsx,mdx}"
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
     extend: {}
   },
-  plugins: []
+  plugins: [nextui()]
 };
