8a8982d123395971b6678797ca4ffbf54c3de141
Prince Owusu <prince.owusu@hubtel.com>
2024-08-03 09:16:54 +0000

    ✨ feat: Add identity service and HTTP request handling
    - Added `axios` and `query-string` to dependencies for HTTP requests and URL query handling.
    - Introduced `IdentityService` class in `identity-service.tsx` for managing identity operations with Axios integration.
    - Created `Result` class and subclasses in `results.tsx` to process and categorize API responses and errors.
    - Defined TypeScript types for identity forms and models in `identity.tsx`.
    - Configured and exported instances and types in `index.tsx` for use across the application.

---
+++ src/Next-Solution.ExpoApp/services/types/identity.tsx
@@ -0,0 +1,71 @@
+export type CreateAccountForm = {
+  firstName: string;
+  lastName?: string;
+  username: string;
+  usernameType?: "email" | "phoneNumber";
+  password: string;
+};
+
+export type ConfirmAccountForm = {
+  username: string;
+  usernameType?: "email" | "phoneNumber";
+  code?: string;
+  process: "sendCode" | "verifyCode";
+};
+
+export type ChangeAccountForm = {
+  newUsernameType?: "email" | "phoneNumber";
+  newUsername: string;
+  code?: string;
+  process: "sendCode" | "verifyCode";
+};
+
+export type ChangePasswordForm = {
+  oldPassword?: string;
+  newPassword: string;
+  confirmPassword: string;
+};
+
+export type ResetPasswordForm = {
+  username: string;
+  usernameType?: "email" | "phoneNumber";
+  code?: string;
+  process: "sendCode" | "verifyCode";
+};
+
+export type SignInForm = {
+  username: string;
+  usernameType?: "email" | "phoneNumber";
+  password: string;
+};
+
+export type SignInWithProvider = "Google" | "Facebook";
+
+export type RefreshTokenForm = {
+  refreshToken: string;
+};
+
+export type SignOutForm = {
+  refreshToken: string;
+};
+
+export type UserProfileModel = {
+  id: string;
+  firstName: string;
+  lastName?: string;
+  userName: string;
+  email?: string;
+  emailConfirmed: boolean;
+  phoneNumber?: string;
+  phoneNumberConfirmed: boolean;
+  passwordConfigured: boolean;
+  roles: string[];
+};
+
+export type UserSessionModel = UserProfileModel & {
+  tokenType: string;
+  accessToken: string;
+  accessTokenExpiresAt: Date;
+  refreshToken: string;
+  refreshTokenExpiresAt: Date;
+};
