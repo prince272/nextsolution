f11fff3b176e64b558b724fd3cae2ceb7fe68747
Prince Owusu <prince.owusu@hubtel.com>
2024-08-03 06:24:28 +0000

    ✨ feat(Next-Solution.ExpoApp): Install NativeWind

--- src/Next-Solution.ExpoApp/App.tsx
+++ src/Next-Solution.ExpoApp/App.tsx
@@ -1,20 +1,12 @@
+import "./global.css"
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <View className="flex flex-1 items-center justify-center">
+        <Text className="text-base">Open up App.tsx to start working on your app!</Text>
+        <StatusBar style="auto" />
+      </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
